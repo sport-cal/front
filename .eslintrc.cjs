26681c1b35b151577e91423211cac2972b8e08de
ONOUE Origami <onoueorigami@gmail.com>
2023-05-05 16:55:04 +0900

    feat: install tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,25 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es6: true
+  root: true,
+  env: {
+    browser: true,
+    es6: true
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'plugin:storybook/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier'],
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'prettier',
-    ],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 2020,
-        sourceType: 'module'
-    },
-    rules: {
-        'prettier/prettier': 'error',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
-        "react/no-unknown-property": ["error", { "ignore": ["css"] }]
-    }
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    "react/no-unknown-property": ["error", {
+      "ignore": ["css"]
+    }]
+  }
 };
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,17 @@
 import type { Preview } from "@storybook/react";
+import "tailwindcss/tailwind.css";
+import "../src/global.css";
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
+    parameters: {
+        actions: { argTypesRegex: "^on[A-Z].*" },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/
+            }
+        }
+    }
 };
 
 export default preview;
